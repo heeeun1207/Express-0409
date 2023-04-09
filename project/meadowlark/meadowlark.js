1d4a3723cc68908a50b24632c5094bb48876c38a
jungheeeun <jungheeeun272@gmail.com>
2023-04-09 12:24:12 +0900

    app.get  라우트를 추가하는 메서드 =경로

--- project/meadowlark/meadowlark.js
+++ project/meadowlark/meadowlark.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.get('/', (res,req) =>{
+  res.type('text/plain')
+  res.send('Meadowlark Travel');
+})
+app.get('/about', (res,req) =>{
+  res.type('text/plain')
+  res.send('About Meadowlark Travel');
+})
 //custom 404 page 
 app.use ((req,res)=> {
   res.type('text/pain')
